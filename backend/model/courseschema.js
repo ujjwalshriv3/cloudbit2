d97459b95b83f04b42bfe9322404fc5c23016629
ujjwalshriv3 <ujjawalshrivastava698@gmail.com>
2025-11-10 13:25:09 +0530

    update backend

--- backend/model/courseschema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const courseSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  duration: Number // duration in hours
-});
-
-export default mongoose.model("Course", courseSchema);
